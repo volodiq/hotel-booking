e87dd9fc01a3adba25e08aae15ffa90137a9b0b8
Vladimir Boltushev <vladimirboltushev@yandex.ru>
2025-09-22 19:01:08 +0300

    fix(users): Внутреннее API

--- src/contexts/users/api/internal.py
+++ src/contexts/users/api/internal.py
@@ -5,7 +5,6 @@ from shared.utils import calculate_password_hash
 
 from ..core import values
 from ..core.repositories import UserRepository
-from . import internal_dto
 
 
 @dataclass
@@ -22,16 +21,16 @@ class VerifyUserPassword:
         self,
         raw_phone: str,
         raw_password: str,
-    ) -> internal_dto.VerifyUserPasswordOut:
+    ) -> VerifyUserPasswordOut:
         phone = values.PhoneNumber(raw_phone)
 
         user = await self.user_repository.get_user_by_phone(phone)
         if user is None:
-            return internal_dto.VerifyUserPasswordOut(is_valid=False)
+            return VerifyUserPasswordOut(is_valid=False)
 
         password_hash = calculate_password_hash(raw_password)
         is_valid = secrets.compare_digest(user.password_hash, password_hash)
         if not is_valid:
-            return internal_dto.VerifyUserPasswordOut(is_valid=False)
+            return VerifyUserPasswordOut(is_valid=False)
 
-        return internal_dto.VerifyUserPasswordOut(is_valid=True, user_oid=user.oid)
+        return VerifyUserPasswordOut(is_valid=True, user_oid=user.oid)
