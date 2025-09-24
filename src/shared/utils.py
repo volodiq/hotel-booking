4c32512edd30140013ea9173e0a8ac1fb48f56f5
Vladimir Boltushev <vladimirboltushev@yandex.ru>
2025-09-24 22:01:40 +0300

    feat: Добавлен декоратор для внедрения зависимостей в обработчики asyncclick

--- src/shared/utils.py
+++ src/shared/utils.py
@@ -1,5 +1,24 @@
 import datetime
 from functools import partial
 
+import asyncclick
+from dishka import Scope
+from dishka.integrations.base import wrap_injection
+from dishka.integrations.click import CONTAINER_NAME
+
 
 utcnow = partial(datetime.datetime.now, datetime.UTC)
+
+
+def cli_inject(func):
+    """
+    Декоратор для внедрения зависимостей в asyncclick.
+    """
+
+    return wrap_injection(
+        func=func,
+        container_getter=lambda _, __: asyncclick.get_current_context().meta[CONTAINER_NAME],
+        remove_depends=True,
+        is_async=True,
+        scope=Scope.REQUEST,
+    )
