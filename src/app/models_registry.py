26ba717d2d01cb646d94f39f69ac121c8f553275
Vladimir Boltushev <vladimirboltushev@yandex.ru>
2025-09-24 22:28:13 +0300

    feat: Миграция администратора отеля

--- src/app/models_registry.py
+++ src/app/models_registry.py
@@ -2,6 +2,7 @@
 Файл нужен для инициализации всех моделей.
 """
 
+from contexts.hotel_admins.infra.models import HotelAdmin
 from contexts.users.infra.models import UserModel
 from shared.infra.models import DBModel
 
@@ -9,4 +10,5 @@ from shared.infra.models import DBModel
 __all__ = [
     "DBModel",
     "UserModel",
+    "HotelAdmin",
 ]
