e77c6633b22b02ee665685d09ff817f9433cab7e
Vladimir Boltushev <vladimirboltushev@yandex.ru>
2025-10-02 13:21:55 +0300

    refactor(app): Разбиение запуска http сервера

--- src/app/http_server/errors_handlers.py
+++ src/app/http_server/errors_handlers.py
@@ -1,5 +1,6 @@
 from functools import partial
 from http import HTTPStatus
+from typing import NoReturn
 
 from fastapi import FastAPI, HTTPException, Request
 
@@ -7,7 +8,7 @@ from system.security.errors import SecurityException
 from system.seedwork.errors import DomainError
 
 
-def setup_exceptions_handlers(app: FastAPI):
+def setup_exceptions_handlers(app: FastAPI) -> None:
     app.add_exception_handler(DomainError, partial(error_handler, HTTPStatus.BAD_REQUEST))
     app.add_exception_handler(SecurityException, partial(error_handler, HTTPStatus.UNAUTHORIZED))
 
@@ -16,7 +17,7 @@ async def error_handler(
     status_code: HTTPStatus,
     request: Request,
     exc: DomainError | SecurityException,
-) -> None:
+) -> NoReturn:
     raise HTTPException(
         status_code=status_code,
         detail=exc.details,
