2fb31adfcfdddd6bdba4bd60a29e028873bce317
Vladimir Boltushev <vladimirboltushev@yandex.ru>
2025-10-15 09:54:41 +0300

    fix: Абсолютные пути в миграциях

--- migrations/env.py
+++ migrations/env.py
@@ -6,8 +6,8 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from bootstrap import models_registry
-from bootstrap.env import get_env
+from src.bootstrap import models_registry
+from src.bootstrap.env import get_env
 
 
 config = context.config
